9e793472de9500c4a88f7f7baecd2d93b33e3436
nashenol <42261670+nashenol@users.noreply.github.com>
2019-12-03 02:45:18 -0500

    Update script.js

--- page/script.js
+++ page/script.js
@@ -1,5 +1,5 @@
 if (window.location.hostname === 'localhost') {
     window.location.pathname = "/web/snake";
-} else {
+} else if (window.location.hostname === 'nashenol.github.io') {
     window.location.pathname = "/workspace/web/snake";
 }
