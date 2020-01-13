e527ccb767b96e39551bedd47cc2fd541b78fd96
nashenol <42261670+nashenol@users.noreply.github.com>
2020-01-12 20:44:23 -0500

    fixed button navigation and made cellphone friendly

--- page/script.js
+++ page/script.js
@@ -1,5 +1,3 @@
-if (window.location.host.includes('localhost')) {
-    window.location.pathname = "/web/snake";
-} else if (window.location.host.includes('github')) {
-    window.location.pathname = "/workspace/web/snake";
+function navigateTo (page) {
+	window.location.assign("./web/" + page);
 }
