c5b0b659d963ce2b06b1d5e653dd87a97e81ccac
nashenol <42261670+nashenol@users.noreply.github.com>
2020-03-08 23:46:03 -0400

    time setup for functionality
    setup time keeping just to test extent of functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log("KEEP IT SIMPLE!");
+let time = new Date();
+let item = document.createElement("h2");
+item.textContent = "Today is " + time;
+item.classList.add("centerCustom");
+document.body.appendChild(item);
